66bc6da1d8edb816d406c30377427354d6ea5c8a
Andrew Block <andy.block@gmail.com>
2017-05-01 11:27:31 -0400

    Remove oadm dependency

--- test-scripts/install-broker.sh
+++ test-scripts/install-broker.sh
@@ -15,9 +15,9 @@ oc create -f resources/sa.yaml
 # Installer binds “known” roles to SA user
 oc create -f resources/roles.yaml || true
 
-oadm policy add-cluster-role-to-user system:auth-delegator -n ${targetNamespace} -z brokersdk
+oc adm policy add-cluster-role-to-user system:auth-delegator -n ${targetNamespace} system:serviceaccount:${targetNamespace}:brokersdk
 oc create policybinding kube-system -n kube-system
-oadm policy add-role-to-user extension-apiserver-authentication-reader -n kube-system --role-namespace=kube-system system:serviceaccount:${targetNamespace}:brokersdk
+oc adm policy add-role-to-user extension-apiserver-authentication-reader -n kube-system --role-namespace=kube-system system:serviceaccount:${targetNamespace}:brokersdk
 
 # Create the brokersdk replication controller
 oc create -f resources/rc.yaml
