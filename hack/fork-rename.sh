7b9a6b9f55ab4d19106e0bf9296e50034219fad2
craig <cbrookes@redhat.com>
2017-06-26 17:27:44 +0100

    fork-rename.sh did not work as expected on mac osx. Due to differences in the sed

--- hack/fork-rename.sh
+++ hack/fork-rename.sh
@@ -5,7 +5,11 @@ if [[ -z $1 || -z $2 ]]; then
   exit 1
 fi
 
-find . -type f -name *.go -exec sed -i "s#github.com/openshift/open-service-broker-sdk#github.com/$1/$2#g" {} +
-
-sed -i "s#github.com/openshift/open-service-broker-sdk#github.com/$1/$2#g" Makefile
-
+OS_TARGET=`uname -s`  
+BACKUP=""
+if  [ "$OS_TARGET" == "Darwin" ]; then
+  BACKUP="''"
+fi
+  
+find . -type f -name *.go -exec sed -i $BACKUP  "s#github.com/openshift/open-service-broker-sdk#github.com/$1/$2#g" {} +
+  sed -i $BACKUP  "s#github.com/openshift/open-service-broker-sdk#github.com/$1/$2#g" Makefile
